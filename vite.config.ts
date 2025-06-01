c348e405c406a950c0cd041b3fd7e2d51981846a
Kislev Levy <kislev.levy@gmail.com>
2025-06-01 23:54:53 +0300

    V1.4

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/a-vending-machine/',
   plugins: [react()],
   resolve: {
     alias: {
